c2fd85455dce2f005beaa309514e093f8ca535df
italoalves97 <116650470+italoalves97@users.noreply.github.com>
2024-06-30 11:01:47 -0300

    ServidorUDP.py

--- ServidorUDP.py
+++ ServidorUDP.py
@@ -5,27 +5,22 @@ host = "127.0.0.1"
 porta = 1060
 arquivo_dados = 'dados_usuarios.csv'
 
-# Configuração do servidor UDP
 servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 servidor.bind((host, porta))
 print(f"Servidor UDP na escuta ... {host}:{porta}")
 
-# Função para salvar dados no arquivo CSV
 def salvar_dados(nome, cpf, loja, telefone,):
     with open(arquivo_dados, mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([nome, cpf, loja, telefone,])
     print(f"Dados salvos: {nome}, {cpf}, {loja}, {telefone}")
 
-# Função para listar todos os dados salvos no arquivo CSV
 def listar_dados():
     with open(arquivo_dados, mode='r', newline='') as file:
         reader = csv.reader(file)
         dados = [linha for linha in reader]
     return dados
      
-
-# Função para pesquisar dados pelo CPF no arquivo CSV
 def pesquisar_dados(cpf):
     encontrado = False
     with open(arquivo_dados, mode='r', newline='') as file:
@@ -36,7 +31,6 @@ def pesquisar_dados(cpf):
                 return linha
     return None
 
-# Função para excluir dados pelo CPF no arquivo CSV
 def excluir_dados(cpf):
     linhas = []
     excluido = False
@@ -56,22 +50,21 @@ def excluir_dados(cpf):
     else:
         return False
 
-# Loop principal do servidor
 while True:
     dados, endereco = servidor.recvfrom(1024)
     mensagem = dados.decode().split(',')
     
-    if mensagem[0] == '1':  # Incluir Dados
+    if mensagem[0] == '1':
         nome, cpf, loja, telefone, = mensagem[1], mensagem[2], mensagem[3], mensagem[4]
         salvar_dados(nome, cpf, loja, telefone,)
         servidor.sendto("Dados salvos com sucesso.".encode(), endereco)
     
-    elif mensagem[0] == '2':  # Listar Dados
+    elif mensagem[0] == '2':
         dados = listar_dados()
         resposta = "\n".join([f"Nome: {linha[0]}, CPF: {linha[1]}, Loja: {linha[2]}, Telefone: {linha[3]}" for linha in dados])
         servidor.sendto(resposta.encode(), endereco)
     
-    elif mensagem[0] == '3':  # Pesquisar Dados
+    elif mensagem[0] == '3':
         cpf = mensagem[1]
         resultado = pesquisar_dados(cpf)
         if resultado:
@@ -80,7 +73,7 @@ while True:
             resposta = f"Dados com CPF {cpf} não encontrados."
         servidor.sendto(resposta.encode(), endereco)
     
-    elif mensagem[0] == '4':  # Excluir Dados
+    elif mensagem[0] == '4':
         cpf = mensagem[1]
         resultado = excluir_dados(cpf)
         if resultado:
@@ -89,9 +82,8 @@ while True:
             resposta = f"Dados com CPF {cpf} não encontrados."
         servidor.sendto(resposta.encode(), endereco)
 
-    elif mensagem[0] == '5':  # Finalizar
+    elif mensagem[0] == '5':
         servidor.sendto("Servidor UDP Finalizado".encode(), endereco)
         break
 
-# Fechar o servidor UDP
 servidor.close()
